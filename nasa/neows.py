592fdc3772985b51b4c6ed89b6e4d1d2ae455b19
AndrewSzymanek <andrew.szymanek@comcast.net>
2020-09-28 21:06:12 +0000

    NASA NeoWs

--- nasa/neows.py
+++ nasa/neows.py
@@ -12,17 +12,21 @@ def main():
     nasacreds = "api_key=" + nasacreds.strip("\n")        
 
     ## update the date below, if you like
-    startdate = "start_date=2019-11-11"
+    startdate = "start_date=2020-09-21"
 
     ## the value below is not being used in this
     ## version of the script
     # enddate = "end_date=END_DATE"
+    enddate = "end_date=2020-09-28"
 
     # make a request with the request library
-    neowrequest = requests.get(NEOURL + startdate + "&" + nasacreds)
+    neowrequest = requests.get(NEOURL + startdate + "&" +enddate + "&" + nasacreds)
 
     # strip off json attachment from our response
     neodata = neowrequest.json()
+    
+    mostdangerous = []
+    # iterate over each element and add the ones that say "is_potentially_hazardous_asteroid": true
 
     ## display NASAs NEOW data
     print(neodata)
